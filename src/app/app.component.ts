84b7eb598db6d3467bb8368a170cc257a3edeabf
Kiko <kikoguirado@gmail.com>
2025-04-16 17:35:12 +0200

    feat(layout): add footer component and adjust layout structure for improved styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeroComponent } from './components/hero/hero.component';
 import { CustomizationDemoComponent } from './components/customization-demo/customization-demo.component';
 import { FaqComponent } from './components/faq/faq.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { LayoutComponent } from './components/layout/layout.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LayoutComponent, HeroComponent, CustomizationDemoComponent, FaqComponent],
+  imports: [
+    RouterOutlet, 
+    LayoutComponent, 
+    HeroComponent, 
+    CustomizationDemoComponent, 
+    FaqComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
