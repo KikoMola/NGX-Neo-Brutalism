b2be8042ea25b4d176b007b7502cb48624f87025
Kiko <kikoguirado@gmail.com>
2025-04-18 16:39:46 +0200

    feat(accordion): implement tabbed interface for accordion component with preview and code snippets
    - Updated accordion.component.html to introduce a tabbed interface allowing users to switch between a preview and code snippets for the accordion component.
    - Enhanced accordion.component.ts by adding state management for the active tab, improving user interaction and experience.
    - Integrated reusable components for displaying the preview and code snippets, ensuring a cleaner and more maintainable code structure.
    - This update aims to provide a more organized and user-friendly way to explore the accordion functionalities, aligning with neobrutalist design principles while enhancing developer usability.

--- src/app/components/pieces/accordion/accordion.component.ts
+++ src/app/components/pieces/accordion/accordion.component.ts
@@ -1,13 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { CodeSnippetPreviewComponent } from '../../code-snippet-preview/code-snippet-preview.component';
+import { ComponentPreviewComponent } from '../../component-preview/component-preview.component';
+import { ThemeService } from '../../../services/theme.service';
 
 @Component({
   selector: 'app-accordion',
-  imports: [],
+  imports: [
+    CommonModule,
+    ComponentPreviewComponent,
+    CodeSnippetPreviewComponent,
+  ],
   templateUrl: './accordion.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccordionComponent implements OnInit {
+  accordionActiveTab: 'preview' | 'code' = 'preview';
 
-  ngOnInit(): void { }
+  constructor(public themeService: ThemeService) {}
 
+  ngOnInit(): void {}
+
+  // Método para cambiar la pestaña activa
+  setPrimaryButtonTab(tab: 'preview' | 'code'): void {
+    this.accordionActiveTab = tab;
+  }
+
+  get accordionExampleCode(): string {
+    const borderRadiusClass = this.themeService.getBorderRadiusClass();
+    const shadowStyle = this.themeService.getShadowClassForElements();
+    const primaryBgClass = this.themeService.getPrimaryBgClass();
+    const hoverXClass = this.themeService.getHoverTranslateXClass();
+    const hoverYClass = this.themeService.getHoverTranslateYClass();
+
+    const rawHtml = `
+<h1>Hola</h1>`;
+
+    return this.escapeHtml(rawHtml.trim());
+  }
+
+  private escapeHtml(unsafe: string): string {
+    return unsafe
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#039;');
+  }
 }
